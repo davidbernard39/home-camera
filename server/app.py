161ce2c8df9899b109859d2771cef70d6eb4808d
dav <dav@dav-zenbook.home>
2016-08-31 19:11:21 +0200

    Update server with camera streaming

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, Response
-from camera import Camera
+from picamera import PiCamera
 
 app = Flask(__name__)
 
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen(camera):
+def gen(picamera):
     while True:
-        frame = camera.get_frame()
+        frame = picamera.get_frame()
         yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(PiCamera()), mimetype='multipart/x-mixed-replace; boundary')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
