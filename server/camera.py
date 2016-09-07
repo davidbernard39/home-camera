6d14bb2c5895f30d4600c92b6bb5515005b16e61
dav <dav@dav-zenbook.home>
2016-09-07 16:33:16 +0200

    connect to raspberry pi camera

--- server/camera.py
+++ server/camera.py
@@ -1,8 +1,52 @@
-from time import time
+import time
+import io
+import threading
+import picamera
 
 class Camera(object):
-    def __init__(self):
-        self.frames = [open('/home/pi/Pictures/cam' + f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+    thread = None # Background thread that reads frames from camera
+    frame = None # current frame is stored here by background thread
+    last_access = 0 # time of last client access to the camera
+
+    def initialize(self):
+        if Camera.thread is None:
+            # start background frame thread
+            Camera.thread = threading.Thread(target=self._thread)
+            Camera.thread.start()
+
+            # wait until frame is available
+            while self.frame is None:
+                time.sleep(0)
 
     def get_frame(self):
-        return self.frames[int(time()) % 3]
+        Camera.last_access = time.time()
+        self.initialize()
+        return self.frame
+
+    @classmethod
+    def _thread(cls):
+        with picamera.PiCamera() as camera:
+            # camera setup
+            camera.resolution = (320, 240)
+            camera.hflip = True
+            camera.vflip = True
+
+            # let camera warmup
+            camera.start_preview()
+            time.sleep(2)
+
+            stream = io.BytesIO()
+            for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                # store frame
+                stream.seek(0)
+                cls.frame = stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
+
+                # if there hasn't been any client in the last 10 seconds stop
+                # the thread
+                if time.time() - cls.last_access > 10:
+                    break
+        cls.thread = None
