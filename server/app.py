a5566fe88c9901519faa87134271c2ea4a4580cd
dav <dav@dav-zenbook.home>
2016-09-07 15:08:25 +0200

    fix camera with fake camera

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, Response
-from picamera import PiCamera
+from camera import Camera
 
 app = Flask(__name__)
 
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen(picamera):
+def gen(camera):
     while True:
-        frame = picamera.get_frame()
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(PiCamera()), mimetype='multipart/x-mixed-replace; boundary')
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
